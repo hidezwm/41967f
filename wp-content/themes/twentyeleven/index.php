c2be37547ae3b39259646983c9eec7c4f9a873cb
Dezhi Liu <dynosure@gmail.com>
2012-03-15 13:54:54 +0100

    margin and border radius for mobile devices

--- wp-content/themes/twentyeleven/index.php
+++ wp-content/themes/twentyeleven/index.php
@@ -24,7 +24,7 @@ get_header(); ?>
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', get_post_format() ); ?>
+					<?php /* Replace the style */ ?>
 
 				<?php endwhile; ?>
 
