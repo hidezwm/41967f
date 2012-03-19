ef9828bedf0e6fe1b278525016a10ad666727fb0
Dezhi Liu <hidezwm@gmail.com>
2012-03-19 23:47:02 +0100

    css minify

--- wp-content/themes/twentyeleven/header.php
+++ wp-content/themes/twentyeleven/header.php
@@ -46,7 +46,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link type="text/css" rel="stylesheet" media="all" href="/minify/min/f=wp-content/themes/twentyeleven/style.css" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
